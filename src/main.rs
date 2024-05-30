8a2d4c892911830e9839c390c9fc9562e804642e
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-30 15:42:38 +0400

    exit command

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,37 @@ fn main() {
             // ctrl+d - exit
             break;
         }
-        
+
         let input = input.trim();
         if input.len() == 0 {
             // ignore empty lines
             continue;
         }
-        
-        let (command, _rest) = input.split_once(' ').unwrap_or((input, ""));
-        println!("{}: command not found", command)
+
+        let (command, params) = split_input(input);
+        let params = params.trim();
+        match command {
+            "exit" => process_exit(params),
+            _ => eprintln!("{command}: command not found")
+        }
     }
+}
+
+fn split_input(input: &str) -> (&str, &str) {
+    let (head, tail) = input.split_once(' ').unwrap_or((input, ""));
+    (head, tail.trim())
+}
+
+fn process_exit(params: &str) {
+    let (exit_code, _) = split_input(params);
+    let exit_code = if exit_code.len() == 0 {
+        0
+    } else {
+        let Ok(res) = exit_code.parse() else {
+            eprintln!("exit: {exit_code}: invalid integer");
+            return;
+        };
+        res
+    };
+    std::process::exit(exit_code);
 }
