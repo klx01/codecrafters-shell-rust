6a9c18ccf349485eefd265baf7e2b1ebf2fedd74
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-30 15:42:34 +0400

    main loop, handle empty input, handle unknown commands

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use std::io::{self, Write};
 
 fn main() {
-    // prompt
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // prompt
+        print!("$ ");
+        io::stdout().flush().expect("failed to flush prompt");
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).expect("failed to read input");
+        if input.len() == 0 {
+            // ctrl+d - exit
+            break;
+        }
+        
+        let input = input.trim();
+        if input.len() == 0 {
+            // ignore empty lines
+            continue;
+        }
+        
+        let (command, _rest) = input.split_once(' ').unwrap_or((input, ""));
+        println!("{}: command not found", command)
+    }
 }
